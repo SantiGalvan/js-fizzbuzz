5ad78bcab546341dd223b381e10c03340dfab84c
SantiGalvan <santiagogalvancolorado@gmail.com>
2023-12-20 17:32:04 +0100

    create for

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log('JS OK');
+
+/*
+Scompongo il problema
+
+Prima parte: Lo mostro in console
+- 1 Creo il cliclo for e stampo in console i numeri da 1 a 100
+- 2 Creo una variabile che contenga i miei numeri
+- 3 I multipli di 3 verranno sostituiti da Fizz
+- 4 I multipli di 5 verranno sostituiti da Buzz
+- 5 I multipli di 3 e 5 verranno sostituiti da FizzBuzz
+- 6 Stampo in console la variabile numbers
+
+Seconda parte: Lo mostro nel DOM
+*/
+
+// - 1 Creo il ciclo for per stampare i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    let numbers = i;
+    console.log(numbers);
+}
