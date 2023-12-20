accb63b8fbf134449c617111b422cffbc9d74567
SantiGalvan <santiagogalvancolorado@gmail.com>
2023-12-20 17:34:59 +0100

    complete for

--- js/script.js
+++ js/script.js
@@ -16,6 +16,26 @@ Seconda parte: Lo mostro nel DOM
 
 // - 1 Creo il ciclo for per stampare i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
+    // - 2 Creo una variabile che contenga i miei numeri
     let numbers = i;
+
+    // - 3 I multipli di 3 verranno sostituiti da Fizz
+    if (i % 3 == 0) {
+        numbers = 'Fizz'
+    }
+
+    // - 4 I multipli di 5 verranno sostituiti da Buzz
+    if (i % 5 == 0) {
+        numbers = 'Buzz'
+    }
+
+    // - 5 I multipli di 3 e 5 verranno sostituiti da FizzBuzz
+    if (i % 3 == 0 && i % 5 == 0) {
+        numbers = 'FizzBuzz'
+    }
+
+    // - 6 Stampo in console la variabile numbers
     console.log(numbers);
+
+
 }
